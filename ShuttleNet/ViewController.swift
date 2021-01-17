caf13a63659fb02e743c1003ee51eccc74b62608
yusuke.saito <yusuke009ag@gmail.com>
2021-01-17 21:20:30 +0900

    Initial Commit

---
+++ ShuttleNet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShuttleNet
+//
+//  Created by 齋藤友祐 on 2021/01/17.
+//  Copyright © 2021 yusuke.saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
